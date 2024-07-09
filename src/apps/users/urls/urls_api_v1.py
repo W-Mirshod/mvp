e586b8f56bf47289ef1a19a14cb3e428f95f54ea
Denis <denisnesterov15@gmail.com>
2024-07-09 11:59:47 +0400

    adapted to the PEP8 standard

--- src/apps/users/urls/urls_api_v1.py
+++ src/apps/users/urls/urls_api_v1.py
@@ -1,19 +1,29 @@
 from django.urls import path
 
-from ..views import LoginTokenView, RefreshTokenView, RegistrationViewSet
+from .. import views
 
 app_name = "users_api"
 
 urlpatterns = [
-    path("login/", LoginTokenView.as_view({"post": "login"}), name="token_obtain_pair"),
+    path(
+        "login/",
+        views.LoginTokenView.as_view({"post": "login"}),
+        name="token_obtain_pair",
+    ),
     path(
         "token_refresh/",
-        RefreshTokenView.as_view({"post": "refresh"}),
+        views.RefreshTokenView.as_view({"post": "refresh"}),
         name="token_refresh",
     ),
     path(
         "registration/",
-        RegistrationViewSet.as_view({"post": "registration"}),
+        views.RegistrationViewSet.as_view({"post": "registration"}),
         name="registration",
     ),
+]
+
+views = [
+    views.LoginTokenView,
+    views.RegistrationViewSet,
+    views.RefreshTokenView,
 ]
