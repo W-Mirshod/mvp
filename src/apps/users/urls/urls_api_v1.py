86b9e1df87f4f874607c8e58013c4f984419aa17
Denis <denisnesterov15@gmail.com>
2024-07-09 11:18:59 +0400

    change path urls_api_v1.py

--- src/apps/users/urls/urls_api_v1.py
+++ src/apps/users/urls/urls_api_v1.py
@@ -7,7 +7,7 @@ app_name = "users_api"
 
 urlpatterns = [
     path("login/", views.LoginTokenView.as_view({"post": "login"}), name="token_obtain_pair"),
-    path("api/1.0/token_refresh/", views.RefreshTokenView.as_view({"post": "refresh"}), name="token_refresh"),
+    path("token_refresh/", views.RefreshTokenView.as_view({"post": "refresh"}), name="token_refresh"),
     path(
         "registration/",
         views.RegistrationViewSet.as_view({"post": "registration"}),
