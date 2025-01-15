97efd01d9672f99a90f625e86ee943128a953e9f
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-15 23:11:09 +0200

    fix(src/settings/urls): fix imports

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from .yasg import urlpatterns as doc_url
+from src.settings.yasg import urlpatterns as doc_url
 
 urlpatterns = (
     [
