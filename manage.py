d171814c2a5d930fd60ce873f18f1c5cf3a09f30
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-16 20:53:51 +0200

    fix(manage.py): fix structure

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings.prod")
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.config.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
