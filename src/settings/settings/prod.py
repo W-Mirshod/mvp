1681a34f192d30ce8bd8b901749eb040b6d40318
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-16 16:09:01 +0200

    fix(src/settings/settings/prod): fix variables

--- src/settings/settings/prod.py
+++ src/settings/settings/prod.py
@@ -1,8 +1,9 @@
-from .base import *
+
+from src.settings.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", False)
-SQL_DEBUG = env.bool("SQL_DEBUG", False)
+DEBUG = environ_values.get("DEBUG")
+SQL_DEBUG = environ_values.get("SQL_DEBUG")
 
 if SQL_DEBUG:
     MIDDLEWARE += ["utils.middleware.DebugQuerysetsWare"]
@@ -13,61 +14,49 @@ if DEBUG:
     ]
 
 
-RUN_IN_DOCKER = env.bool("RUN_IN_DOCKER", False)
-print(f"RUN_IN_DOCKER: {RUN_IN_DOCKER}")
-if not RUN_IN_DOCKER:
-    REDIS_HOST = env.str("REDIS_HOST_LOCAL", "redis")
-    REDIS_PORT = env.str("REDIS_PORT_LOCAL", "6379")
+# RUN_IN_DOCKER = environ_values.get("RUN_IN_DOCKER")
+#
+# if not RUN_IN_DOCKER:
+#     REDIS_HOST = environ_values.get("REDIS_HOST")
+#     REDIS_PORT = environ_values.get("REDIS_PORT")
+#
+# # region Celery
+# BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+# CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
 
-# region Celery
-BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
-CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
-
-print(f"BROKER_URL: {BROKER_URL}")
-print(f"CELERY_BROKER_URL: {CELERY_BROKER_URL}")
 
 CELERY_BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
 CELERY_CACHE_BACKEND = "default"
 CELERY_RESULT_BACKEND = "django-db"
-CELERY_ACCEPT_CONTENT = env.list("CELERY_ACCEPT_CONTENT")
-CELERY_TASK_SERIALIZER = env.str("CELERY_TASK_SERIALIZER", "")
-CELERY_RESULT_SERIALIZER = env.str("CELERY_RESULT_SERIALIZER", "")
-CELERY_TIMEZONE = env.str("CELERY_TIMEZONE", "")
+CELERY_ACCEPT_CONTENT = environ_values.get("CELERY_ACCEPT_CONTENT")
+CELERY_TASK_SERIALIZER = environ_values.get("CELERY_TASK_SERIALIZER")
+CELERY_RESULT_SERIALIZER = environ_values.get("CELERY_RESULT_SERIALIZER")
+CELERY_TIMEZONE = environ_values.get("CELERY_TIMEZONE")
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 # endregion
 
 
 # region Database
-if RUN_IN_DOCKER:
-    DATABASES = {
-        "default": {
-            "ENGINE": env.str("SQL_ENGINE"),
-            "NAME": env.str("SQL_DATABASE"),
-            "USER": env.str("SQL_USER"),
-            "PASSWORD": env.str("SQL_PASSWORD"),
-            "HOST": env.str("SQL_HOST"),
-            "PORT": env.str("SQL_PORT"),
-        }
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": environ_values.get("DB_NAME"),
+        "USER": environ_values.get("DB_USER"),
+        "PASSWORD": environ_values.get("DB_PASSWORD"),
+        "HOST": environ_values.get("DB_HOST"),
+        "PORT": environ_values.get("DB_PORT"),
+        "CONN_MAX_AGE": 30,
+        "CONN_HEALTH_CHECKS": True,
     }
-else:
-    DATABASES = {"default": env.db(var="DATABASE_URL")}
+}
+
 # endregion
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
 
+CSRF_TRUSTED_ORIGINS = environ_values.get("CSRF_TRUSTED_ORIGINS").split(",")
 
-CSRF_TRUSTED_ORIGINS = env.list(
-    "CSRF_TRUSTED_ORIGINS",
-    default=[
-        # "http://localhost",
-        # "http://127.0.0.1",
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://127.0.0.1:3001",
-    ],
-)
 
 if DEBUG:
     import socket
@@ -87,3 +76,16 @@ SWAGGER_SETTINGS = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+
+"""Email settings ->"""
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587  # 587 for TLS, 465 for SSL
+EMAIL_HOST_USER = environ_values.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = environ_values.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = environ_values.get("DEFAULT_FROM_EMAIL", "")
+"""<- Email settings"""
+
