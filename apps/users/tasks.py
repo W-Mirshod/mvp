cd3e4dd2c71526092e0ac2d186433734680f63ae
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-16 23:08:12 +0200

    fix(apps/users/tasks): add annotation to project

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -1,22 +1,23 @@
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
-from rest_framework.reverse import reverse_lazy
+from rest_framework.reverse import reverse_lazy, reverse
 
 
 @shared_task
-def send_verification_email_task(from_email, user_email, token):
+def send_verification_email_task(from_email: str, user_email: str, token: str) -> None:
     subject = "Email verification"
     message = "Thank you for registering on our website. Please verify your email:\n"
-    link = (
-        settings.MAIN_HOST + reverse_lazy("users_api:email_verify") + "?token=" + token
-    )
+
+    verification_url = str(reverse_lazy("users_api:email_verify"))
+    link = settings.MAIN_HOST + verification_url + "?token=" + token
+
     recipient_list = (user_email,)
     send_mail(subject, message + link, from_email, recipient_list)
 
 
 @shared_task
-def send_one_time_jwt_task(from_email: str, user_email: str, token: str):
+def send_one_time_jwt_task(from_email: str, user_email: str, token: str) -> None:
     subject = "Reset password"
     message = "The link for reset password:\n"
     link = (
