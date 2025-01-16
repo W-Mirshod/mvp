7f9043fb9aca1a27d1fd3a2075fb3484ca4219d4
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-16 16:09:53 +0200

    fix(src/settings/celery): fix celery config

--- src/settings/celery.py
+++ src/settings/celery.py
@@ -1,6 +1,18 @@
+import os
 from celery import Celery
-from django.conf import settings
+from dotenv import dotenv_values
 
-app = Celery("mm_backend")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+environ_values = dotenv_values(".env")
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src')
+
+app = Celery('mm_backend')
+CELERY_BROKER_URL = environ_values.get("CELERY_BROKER_URL")
+app.conf.broker_url = CELERY_BROKER_URL
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Автоматическое обнаружение задач
+app.autodiscover_tasks()
+
