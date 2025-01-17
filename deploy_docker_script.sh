be509a40bec68504ca3f424b14f9142525c7f1ab
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-17 22:59:48 +0200

    add(deploy_docker_script.sh): add script for deploy

---
+++ deploy_docker_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "--- Update Python image ---"
+docker pull python:3.12
+sleep 0.1
+
+echo "--- Update Redis image ---"
+docker pull redis:alpine
+sleep 0.1
+
+echo "--- Update Postgres image ---"
+docker pull postgres:latest
+sleep 0.1
+
+echo "--- Build docker compose ---"
+docker compose -f docker-compose.yml
+sleep 0.1
+
+echo "--- Check docker images ---"
+docker images
+sleep 0.1
+
+echo "--- Start docker compose ---"
+docker compose -f docker-compose.yml  up -d
+sleep 0.1
