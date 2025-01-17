972aeab452e9cff9f2482677bf764c663c922483
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-18 00:20:07 +0200

    fix(deploy_docker_script): fix docker build command

--- deploy_docker_script.sh
+++ deploy_docker_script.sh
@@ -13,7 +13,7 @@ docker pull postgres:latest
 sleep 0.1
 
 echo "--- Build docker compose ---"
-docker compose -f docker-compose.yml
+docker compose -f docker-compose.yml build
 sleep 0.1
 
 echo "--- Check docker images ---"
@@ -21,5 +21,5 @@ docker images
 sleep 0.1
 
 echo "--- Start docker compose ---"
-docker compose -f docker-compose.yml  up -d
+docker compose -f docker-compose.yml up -d
 sleep 0.1
