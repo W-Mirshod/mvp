088d4d1be3c15b135693b0d34dd075984035d007
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-18 02:56:48 +0200

    fix(config/settings): comment cors & csrf

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-    "corsheaders",
+   # "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "django_celery_results",
@@ -47,11 +47,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+  #  "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+  #  "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
