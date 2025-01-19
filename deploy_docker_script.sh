e1c6b64a6f27e6389f027f0d3e49b978e909d082
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-19 19:04:47 +0200

    add(deploy_docker_script.sh): add prometheus & grafana to deploy script

--- deploy_docker_script.sh
+++ deploy_docker_script.sh
@@ -8,12 +8,24 @@ echo "--- Update Redis image ---"
 docker pull redis:latest
 sleep 0.1
 
+echo "--- Update Grafana image ---"
+docker pull grafana/grafana
+sleep 0.1
+
+echo "--- Update Prometheus image ---"
+docker pull prom/prometheus
+sleep 0.1
+
 echo "--- Update Postgres image ---"
 docker pull postgres:latest
 sleep 0.1
 
+echo "--- Update Node Exporter image ---"
+docker pull prom/node-exporter
+sleep 0.1
+
 echo "--- Build docker compose ---"
-docker compose -f docker-compose.yml build
+docker compose -f docker-compose.yml -f docker-compose-monitoring.yml build
 sleep 0.1
 
 echo "--- Check docker images ---"
@@ -21,5 +33,5 @@ docker images
 sleep 0.1
 
 echo "--- Start docker compose ---"
-docker compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml -f docker-compose-monitoring.yml up -d
 sleep 0.1
