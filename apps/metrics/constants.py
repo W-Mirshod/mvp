1346868020380099ca5db49191c65fbf4706ac1f
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-19 15:36:32 +0200

    add(apps/metrics/constants): add constants for metrics

---
+++ apps/metrics/constants.py
@@ -0,0 +1,6 @@
+class MonitoringConstants:
+
+    CONNECTED_SYSTEM_STATUS = "connected"
+    DISCONNECTED_SYSTEM_STATUS = "disconnected"
+    HEALTHY_OVERALL_SYSTEM_STATUS = "healthy"
+    UNHEALTHY_OVERALL_SYSTEM_STATUS = "unhealthy"
