be63f25f4ed0f81d61383cac384bbebd078ecff3
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-24 01:58:19 +0200

    add(apps/users/apps): add user health check to app

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from health_check.plugins import plugin_dir
+
+from apps.users.health_check.v1.hc_user import UserHealthCheck
 
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.users"
+
+    def ready(self):
+
+        plugin_dir.register(UserHealthCheck)
