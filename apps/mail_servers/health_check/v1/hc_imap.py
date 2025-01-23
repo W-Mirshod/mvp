89428a9bd7a8dc64a8570450118190576049513e
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-24 01:36:08 +0200

    add(apps/mail_servers/health_check/v1/hc_imap): add imap api health check

---
+++ apps/mail_servers/health_check/v1/hc_imap.py
@@ -0,0 +1,38 @@
+from health_check.backends import BaseHealthCheckBackend
+from rest_framework.reverse import reverse_lazy
+from rest_framework import status
+
+from apps.sentry.sentry_constants import SentryConstants
+from apps.sentry.sentry_scripts import SendToSentry
+from config.settings import MAIN_HOST
+import requests
+
+
+class IMAPHealthCheck(BaseHealthCheckBackend):
+
+    critical_service = False
+
+    def check_status(self) -> bool:
+        try:
+            url = MAIN_HOST + reverse_lazy("servers_api:imap-server_list")
+            response = requests.get(url=url, timeout=5)
+
+            if response.status_code != status.HTTP_401_UNAUTHORIZED:
+                raise Exception(f"Unexpected status code: {response.status_code}")
+
+            return True
+        except Exception as ex:
+            SendToSentry.send_scope_msg(
+                scope_data={
+                    "message": f"IMAPHealthCheck.check_status: Exception",
+                    "level": SentryConstants.SENTRY_MSG_ERROR,
+                    "tag": SentryConstants.SENTRY_TAG_REQUEST,
+                    "detail": f"IMAP API health check failed",
+                    "extra_detail": str(ex),
+                }
+            )
+            self.add_error(str(ex))
+            return False
+
+    def identifier(self):
+        return self.__class__.__name__
