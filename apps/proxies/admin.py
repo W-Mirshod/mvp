647c9cf147221d0baa7d10fbe646d1780720a31b
shoxxd221 <shoxxd@mail.ru>
2025-01-23 20:33:38 +0300

    Added proxy migration and admin

--- apps/proxies/admin.py
+++ apps/proxies/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.proxies import Proxy
+
+
+@admin.register(Proxy)
+class ProxyAdmin(admin.ModelAdmin):
+    list_display = (
+        "host",
+        "port",
+        "is_active",
+        "country",
+        "country_code",
+        "anonymity",
+        "timeout",
+        "username",
+        "password",
+        "user",
+    )
+    list_filter = (
+        "is_active",
+        "country",
+        "timeout",
+    )
+    search_fields = (
+        "is_active",
+        "user",
+        "anonymity",
+    )
+    ordering = ("-id",)
