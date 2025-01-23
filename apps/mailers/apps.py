d0acb84cebac6aca8a5efddebd7fa75847b37e0c
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-24 01:47:32 +0200

    add(apps/mailers/apps): add health checkers to app

--- apps/mailers/apps.py
+++ apps/mailers/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from health_check.plugins import plugin_dir
+
+from apps.mailers.health_check.v1.hс_sent_message import SentMessagesHealthCheck
+from apps.mailers.health_check.v1.hс_message_template import MessageTemplateHealthCheck
 
 
 class MailersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.mailers"
+
+    def ready(self):
+
+        plugin_dir.register(SentMessagesHealthCheck)
+        plugin_dir.register(MessageTemplateHealthCheck)
