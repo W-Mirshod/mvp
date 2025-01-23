938bc0b1939456b22c6eaf4a6312e05ab6ec1728
shoxxd221 <shoxxd@mail.ru>
2025-01-23 21:34:32 +0300

    Edited proxy viewset and added proxy urls

---
+++ apps/proxies/urls/urls_proxies.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from apps.proxies.views.v1.views_proxies import ProxyViewSet
+
+router = DefaultRouter()
+router.register('', ProxyViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- apps/proxies/views/v1/views_proxies.py
+++ apps/proxies/views/v1/views_proxies.py
@@ -1,11 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from apps.proxies.models.proxies import Proxy
 from apps.proxies.serializers.proxy import ProxySerizalizer
+from utils.permissions import IsTokenValid
 
 
 class ProxyViewSet(ModelViewSet):
     queryset = Proxy.objects.all()
     serializer_class = ProxySerizalizer
     pagination_class = PageNumberPagination
+    permission_classes = (IsAuthenticated, IsTokenValid)
