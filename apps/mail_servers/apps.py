751e9cb400170ed23842534a5a9817cd404320a9
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-24 01:37:42 +0200

    add(apps/mail_servers/apps): add health checkers to app

--- apps/mail_servers/apps.py
+++ apps/mail_servers/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from health_check.plugins import plugin_dir
+from apps.mail_servers.health_check.v1.hc_imap import IMAPHealthCheck
+from apps.mail_servers.health_check.v1.hс_smtp import SMTPHealthCheck
+from apps.mail_servers.health_check.v1.hc_proxy import ProxyHealthCheck
 
 
 class MailServersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.mail_servers"
+
+    def ready(self):
+
+        plugin_dir.register(IMAPHealthCheck)
+        plugin_dir.register(SMTPHealthCheck)
+        plugin_dir.register(ProxyHealthCheck)
