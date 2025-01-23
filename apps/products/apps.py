678c31728bf61ba4fd197ebcb8c3efc046bae0b4
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-24 01:54:07 +0200

    add(apps/products/apps): add health checkers to app

--- apps/products/apps.py
+++ apps/products/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from health_check.plugins import plugin_dir
+
+from apps.products.health_check.v1.hc_product import ProductHealthCheck
+from apps.products.health_check.v1.hc_tariff import TariffHealthCheck
 
 
 class ProductsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.products"
+
+    def ready(self):
+
+        plugin_dir.register(ProductHealthCheck)
+        plugin_dir.register(TariffHealthCheck)
