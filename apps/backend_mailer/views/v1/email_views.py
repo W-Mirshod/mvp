f9ddd3efb4bd5c2efc2f982069bce27bf0c17bbd
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-29 01:10:45 +0200

    feat(app/backend_mailer/views/v1/email_views): add views for email

---
+++ apps/backend_mailer/views/v1/email_views.py
@@ -0,0 +1,30 @@
+from rest_framework.permissions import IsAuthenticated
+
+from apps.backend_mailer.models import Email
+from apps.backend_mailer.serializers.email_serializers import (
+    CreateEmailSerializer,
+    RetrieveEmailSerializer,
+)
+from apps.backend_mailer.view_logic.email_queryset import EmailQueryset
+from utils.permissions import IsTokenValid
+from utils.views import MultiSerializerViewSet
+
+
+class SentMessageView(MultiSerializerViewSet):
+    queryset = Email.objects.all()
+    permission_classes = (
+        IsAuthenticated,
+        IsTokenValid,
+    )
+    serializers = {
+        "list": RetrieveEmailSerializer,
+        "create": CreateEmailSerializer,
+        "retrieve": RetrieveEmailSerializer,
+    }
+
+    def get_queryset(self):
+        return EmailQueryset.email_queryset(
+            user_obj=self.request.user,
+            action=self.action,
+            kwargs=self.kwargs,
+        )
