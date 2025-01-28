5dac8537712692f6d601e3893232880bc9fbcba2
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-29 00:15:13 +0200

    feat(apps/backend_mailer/models/log): add model for log

---
+++ apps/backend_mailer/models/__init__.py
@@ -0,0 +1,5 @@
+from apps.backend_mailer.models.email import Email
+from apps.backend_mailer.models.attachment import Attachment
+from apps.backend_mailer.models.email_template import EmailTemplate
+from apps.backend_mailer.models.log import Log
+from apps.backend_mailer.models.backend import EmailBackend
