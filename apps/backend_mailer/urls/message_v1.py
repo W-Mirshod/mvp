4147cd24627cd0f8203c0e25c660bb99598b0951
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-29 15:52:55 +0200

    feat(app/backend_mailer/urls/message_v1): add urls for backend

--- apps/backend_mailer/urls/message_v1.py
+++ apps/backend_mailer/urls/message_v1.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from apps.backend_mailer.views.v1.email_views import SentMessageView
-from apps.backend_mailer.views.v1.email_tamplate_views import EmailTemplateView
+from apps.backend_mailer.views.v1.email_template_views import EmailTemplateView
+from apps.backend_mailer.views.v1.email_backend import EmailBackendView
+
+
 app_name = "backend_email"
 
 urlpatterns = [
@@ -17,12 +20,22 @@ urlpatterns = [
     ),
     path(
         "message-templates/",
-        EmailTemplateView.as_view({"get": "list",  "post": "create"}),
+        EmailTemplateView.as_view({"get": "list", "post": "create"}),
         name="message-template_list",
     ),
     path(
         "message-templates/<int:pk>/",
         EmailTemplateView.as_view({"get": "retrieve", "delete": "destroy"}),
         name="message-template_by_id",
+    ),
+    path(
+        "email-backend/",
+        EmailBackendView.as_view({"get": "list", "post": "create"}),
+        name="email-backend_list",
+    ),
+    path(
+        "email-backend/<int:pk>/",
+        EmailBackendView.as_view({"get": "retrieve", "delete": "destroy"}),
+        name="email-backend_by_id",
     ),
 ]
