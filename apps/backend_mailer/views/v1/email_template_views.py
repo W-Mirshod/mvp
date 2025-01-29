6832f1f061eff826da17fd49a97194501c5a2413
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-29 15:48:06 +0200

    feat(app/backend_mailer/views/v1/email_template_views): add view for  email template

---
+++ apps/backend_mailer/views/v1/email_template_views.py
@@ -0,0 +1,29 @@
+from rest_framework.permissions import IsAuthenticated
+
+from apps.backend_mailer.serializers.email_template_serializer import (
+    RetrieveEmailTemplateSerializer,
+    CreateEmailTemplateSerializer,
+)
+from apps.backend_mailer.view_logic.email_template_qs import EmailTemplateQueryset
+from utils.permissions import IsTokenValid
+from utils.views import MultiSerializerViewSet
+
+
+class EmailTemplateView(MultiSerializerViewSet):
+    queryset = None  # see get_queryset
+    permission_classes = (
+        IsAuthenticated,
+        # IsTokenValid,
+    )
+    serializers = {
+        "list": RetrieveEmailTemplateSerializer,
+        "create": CreateEmailTemplateSerializer,
+        "retrieve": RetrieveEmailTemplateSerializer,
+    }
+
+    def get_queryset(self):
+        return EmailTemplateQueryset.email_template_queryset(
+            user_obj=self.request.user,
+            action=self.action,
+            kwargs=self.kwargs,
+        )
