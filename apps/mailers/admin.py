77b3adc16234d00652cc25e37bbaf246ed8c1e9e
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-30 18:30:37 +0200

    feat(apps/mailers/admin): register campaign in admin

--- apps/mailers/admin.py
+++ apps/mailers/admin.py
@@ -1,42 +1,18 @@
 from django.contrib import admin
 
-from apps.mailers.models import MessageTemplate
-from apps.mailers.models.event import Event
-from apps.mailers.models.message import SentMessage
+from apps.mailers.models import Campaign
 
 
-@admin.register(SentMessage)
-class SentMessageAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "template", "results")
-    search_fields = ("user__username", "template")
-    list_filter = ("user",)
-
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    autocomplete_fields = ("author",)
     list_display = (
         "id",
-        "server",
+        "author",
         "status",
-        "sent_message",
+        "message",
         "created_at",
         "updated_at",
     )
-    search_fields = ("server__type", "status", "sent_message__template")
-    list_filter = ("status", "server")
-
-
-@admin.register(MessageTemplate)
-class MessageTemplateAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "from_address",
-        "is_deleted",
-        "created_at",
-        "updated_at",
-    )
-    search_fields = ("from_address",)
-    list_filter = (
-        "from_address",
-        "is_deleted",
-    )
+    search_fields = ("campaign_name", "status", "comment")
+    list_filter = ("status", "shipping_type", "message_type", "country_tag")
