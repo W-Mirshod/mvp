28cc777c79cd4b82baf6f9694ff2bf2d44e3f948
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-30 18:24:00 +0200

    fix(apps/mailers/): delete unused files

--- apps/mailers/serializers/template.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from apps.mailers.models.template import MessageTemplate
-
-
-class MessageTemplateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MessageTemplate
-        fields = ("id", "from_address", "template", "message")
