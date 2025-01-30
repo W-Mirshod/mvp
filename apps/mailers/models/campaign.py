89c6b17e51c09e58964e998543d51103ab5b0b90
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-01-30 22:56:19 +0200

    feat(apps/mailer/models/campaign): update campaign model

--- apps/mailers/models/campaign.py
+++ apps/mailers/models/campaign.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from apps.mailers.constants import EventConstants
+from apps.mailers.constants import CampaignConstants
 from apps.backend_mailer.models import Email
 from apps.users.models import User
 
@@ -14,23 +14,23 @@ class Campaign(DeleteModelMixin, DateModelMixin, models.Model):
         related_name="campaign_to_user",
     )
     campaign_id = models.CharField(
-        max_length=EventConstants.MAX_CAMPAIGN_ID_LENGTH,
+        max_length=CampaignConstants.MAX_CAMPAIGN_ID_LENGTH,
         help_text=_("Campaign it"),
         verbose_name=_("Campaign id"),
     )
     campaign_name = models.CharField(
-        max_length=EventConstants.MAX_CAMPAIGN_NAME_LENGTH,
+        max_length=CampaignConstants.MAX_CAMPAIGN_NAME_LENGTH,
         help_text=_("Campaign name"),
         verbose_name=_("Campaign name"),
     )
     shipping_type = models.PositiveSmallIntegerField(
-        choices=EventConstants.SHIPPING_TYPE_CHOICES,
-        default=EventConstants.SHIPPING.default,
+        choices=CampaignConstants.SHIPPING_TYPE_CHOICES,
+        default=CampaignConstants.SHIPPING.default,
         help_text=_("Campaign shipping type"),
     )
     message_type = models.PositiveSmallIntegerField(
-        choices=EventConstants.MESSAGE_TYPE_CHOICES,
-        default=EventConstants.MESSAGE_TYPE.default,
+        choices=CampaignConstants.MESSAGE_TYPE_CHOICES,
+        default=CampaignConstants.MESSAGE_TYPE.default,
         help_text=_("Campaign message type"),
     )
     comment = models.TextField(help_text=_("Campaign comment"))
@@ -40,8 +40,8 @@ class Campaign(DeleteModelMixin, DateModelMixin, models.Model):
     )
 
     status = models.PositiveSmallIntegerField(
-        choices=EventConstants.STATUS_CHOICES,
-        default=EventConstants.STATUS.created,
+        choices=CampaignConstants.STATUS_CHOICES,
+        default=CampaignConstants.STATUS.created,
         help_text=_("Campaign status"),
     )
 
@@ -49,7 +49,7 @@ class Campaign(DeleteModelMixin, DateModelMixin, models.Model):
         Email, on_delete=models.CASCADE, help_text=_("Message for campaign")
     )
     country_tag = models.CharField(
-        max_length=EventConstants.MAX_COUNTRY_TAG_LENGTH,
+        max_length=CampaignConstants.MAX_COUNTRY_TAG_LENGTH,
         help_text=_("Country filed"),
         verbose_name=_("Country filed"),
     )
