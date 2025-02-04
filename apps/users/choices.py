1610bc4d80d63fed260bad39cccfb1b22b6abb96
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-02-04 18:11:02 +0200

    feat(apps/user/choices): add support role

--- apps/users/choices.py
+++ apps/users/choices.py
@@ -1,21 +1,25 @@
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles:
-    """
-    User roles
-    """
+class UserConstance:
 
+    TG_USERNAME_MAX_LENGTH = 255
+
+    """ User roles -> """
     STUFF = "stuff"
     ADMIN = "admin"
     USER = "user"
     OWNER_COMPANY = "owner_company"
+    SUPPORT = "support"
 
-    ITEMS = [STUFF, ADMIN, USER, OWNER_COMPANY]
+    ITEMS = [STUFF, ADMIN, USER, OWNER_COMPANY, SUPPORT]
 
-    CHOICES = (
+    USER_ROLES_CHOICES = (
         (STUFF, _("STUFF")),
         (ADMIN, _("ADMIN")),
         (USER, _("USER")),
         (OWNER_COMPANY, _("OWNER_COMPANY")),
+        (SUPPORT, _("SUPPORT")),
     )
+
+    """ <- User roles"""
