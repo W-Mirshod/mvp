41702f104ee3bac5afea85aa5b68f3c362b6c5a6
AliaksandrSihai <sigai.aleksandr@mail.ru>
2025-02-08 03:30:54 +0200

    fix(apps/proxies/tasks): fix celery tasks

--- apps/proxies/tasks.py
+++ apps/proxies/tasks.py
@@ -1,9 +1,5 @@
 from celery import app
-
-from apps.proxies.models.proxies import Proxy
-from apps.proxies.utils import check_single_proxy
-
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from apps.proxies.logic.check_proxy_health import CheckProxyHealth
 import logging
 
 
@@ -12,13 +8,4 @@ logger = logging.getLogger(__name__)
 
 @app.shared_task
 def check_proxy_health():
-    proxies = Proxy.objects.all()
-
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        future_to_proxy = {executor.submit(check_single_proxy, proxy): proxy for proxy in proxies}
-        for future in as_completed(future_to_proxy):
-            proxy = future_to_proxy[future]
-            try:
-                future.result()
-            except Exception as e:
-                logger.error(f'Error checking proxy {proxy.host}:{proxy.port} - {e}')
+    CheckProxyHealth.check_proxy_health()
